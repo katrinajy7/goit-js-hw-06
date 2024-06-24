a3b27ed1bdbc68ff57a7248d50478849169568b9
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-24 15:14:09 -0700

    const2

--- task-02.js
+++ task-02.js
@@ -1,9 +1,9 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments",];
 
-let ul = document.getElementById("ingredients");
+const ul = document.getElementById("ingredients");
 
 for (let ingredient of ingredients) {
-  let li = document.createElement("li");
+  const li = document.createElement("li");
   li.innerText = ingredient;
   ul.appendChild(li);
 }
