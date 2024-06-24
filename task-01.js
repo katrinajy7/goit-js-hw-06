0f6d325b4dd8a98714aa16e77ae261311f5ff477
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-24 12:11:24 -0700

    task1.1

--- task-01.js
+++ task-01.js
@@ -1,3 +1,9 @@
 let categories = document.querySelectorAll(".item");
 
 console.log(`Number of categories: ${categories.length}`);
+
+// console.log(categories[0].firstElement[]);
+
+for (let category of category) {
+    console.log(`category: ${category.firstElementChild.innerText}`);
+}
