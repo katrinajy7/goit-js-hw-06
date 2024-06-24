143c16b3df7c958cd15347577c57036f33040ff6
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-24 16:50:08 -0700

    task4 done

---
+++ task-04.js
@@ -0,0 +1,18 @@
+let value = document.getElementById("value");
+value.innerText = initialValue;
+
+let incBtn = document.querySelector('[data-action="increment"]');
+let decBtn = document.querySelector('[data-action="decrement"]');
+
+incBtn.onclick = function () {
+    initialValue += 1;
+    value.innerText = initialValue;
+
+};
+
+decBtn.onclick = function () {
+    if (initialValue > 0) {
+        initialValue -=1;
+        value.innerText = initialValue;
+    }
+}
