919fa86781eec5030b65ba83fbc35c036a55a541
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-24 14:46:49 -0700

    2

--- task-02.js
+++ task-02.js
@@ -10,7 +10,7 @@ const ingredients = [
 let ul = document.getElementById("ingredients");
 
 for(let ingredient of ingredients) {
-  let li = document.createElement("li")
-  li.innerText = ingredient
+  let li = document.createElement("li");
+  li.innerText = ingredient;
   ul.appendChild(li);
 }
