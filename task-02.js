4bab4409dcc32c3580c8f32504f3d70b62668204
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-24 15:15:26 -0700

    ,

--- task-02.js
+++ task-02.js
@@ -1,9 +1,9 @@
-const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments",];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
-const ul = document.getElementById("ingredients");
+let ul = document.getElementById("ingredients");
 
 for (let ingredient of ingredients) {
-  const li = document.createElement("li");
+  let li = document.createElement("li");
   li.innerText = ingredient;
   ul.appendChild(li);
 }
