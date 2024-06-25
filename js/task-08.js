90e4fe0fc0627eb7a00a22079ea715d0b4c9d9e2
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-24 19:30:39 -0700

    TASK 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
 
+ const loginForm = document.querySelector('.login-form');
+
+ loginForm.addEventListener('submit', function(event) {
+   event.preventDefault(); 
+
+   const emailInput = this.elements['email'];
+   const passwordInput = this.elements['password'];
+
+   if (!emailInput.value || !passwordInput.value) {
+     alert('Please fill in all fields.');
+     return;
+   }
+
+   const formData = {
+     email: emailInput.value,
+     password: passwordInput.value
+   };
+
+   console.log('Form Data:', formData);
+
+   // Reset the form fields
+   this.reset();
+ });
