e0faf13bdce986d30f37d8f7e63d7b65b45be456
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-24 17:33:45 -0700

    TASK 5 done

---
+++ task-05.js
@@ -0,0 +1,17 @@
+
+
+    const nameInput = document.getElementById('name-input');
+    const nameOutput = document.getElementById('name-output');
+
+    nameInput.addEventListener('input', function() {
+   
+      const inputValue = this.value.trim();
+      
+      if (inputValue === '') {
+        nameOutput.textContent = 'Anonymous';
+      } else {
+        nameOutput.textContent = inputValue;
+      }
+    });
+
+
